75de07c12dcdd1cf4db4e3e963f0b63d9959cdf5
Giovani Mantovani <giovani.mantovanisjc@gmail.com>
2025-06-24 10:03:44 -0300

    Tailwind configuration

---
+++ web/src/app.tsx
@@ -0,0 +1,11 @@
+import logo from './assets/logo.svg';
+
+export function App() {
+  return (
+    <div className="h-dvh flex bg-gray-200 flex-col items-center justify-center p-10">
+      <img src={logo} className="logo react" alt="React logo" />
+
+      <span>test</span>
+    </div>
+  );
+}
