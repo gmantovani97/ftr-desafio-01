4403375c570046a4000c8ed8303a4bd8d7a3ba65
Giovani Mantovani <giovani.mantovanisjc@gmail.com>
2025-06-24 10:36:14 -0300

    Configured absolute imports

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      assets: resolve(__dirname, './src/assets'),
+      components: resolve(__dirname, './src/components'),
+      pages: resolve(__dirname, './src/pages'),
+    },
+  },
 });
