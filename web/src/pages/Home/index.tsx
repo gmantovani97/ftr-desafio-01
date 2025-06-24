ca4aaf114b9b51ae64953373b35df2cd1b76a375
Giovani Mantovani <giovani.mantovanisjc@gmail.com>
2025-06-24 10:30:11 -0300

    Router configuration

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
-import logo from './assets/logo.svg';
+import logo from '../../assets/logo.svg';
 
-export function App() {
+export function Home() {
   return (
     <div className="h-dvh flex bg-gray-200 flex-col items-center justify-center p-10">
       <img src={logo} className="logo react" alt="React logo" />
